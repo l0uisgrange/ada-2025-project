8b7086c4d55c96e1b6ef732dcc76604799b7edc3
badralmahouri <badrglob0@gmail.com>
2025-10-27 16:58:46 +0100

    restructure project following template 2

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 # Data directories
 DATA_DIR = os.path.join(PROJECT_ROOT, "data")
-RESULTS_DIR = os.path.join(PROJECT_ROOT, "results")
+#RESULTS_DIR = os.path.join(PROJECT_ROOT, "results")
 
 # Paths to data files
 BODY_HYPERLINKS_DATA = os.path.join(DATA_DIR, "soc-redditHyperlinks-body.tsv")
