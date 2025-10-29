2fb6017305a92ab27fa59d01d8a759501d5a391c
Louis <70532216+l0uisgrange@users.noreply.github.com>
2025-10-29 12:06:17 +0100

    simplified to cache

--- src/data.py
+++ src/data.py
@@ -2,48 +2,44 @@ import pandas as pd
 import kagglehub
 import os
 
+def download_data():
+    """
+    Downloads all required datasets from Kaggle and returns the path to the downloaded files.
+    """
+
+    return kagglehub.dataset_download("fejwiehf3928uhcwa/ada-2025-project-bald")
+
 def load_data():
     """
-    Downloads and loads all required datasets as pandas DataFrames.
+    Loads all required datasets as pandas DataFrames from the Kaggle path.
     """
 
-    # Downloads dataset from Kaggle
-    destination_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data'))
-    path = kagglehub.dataset_download("fejwiehf3928uhcwa/ada-2025-project-bald", force_download=True)
-
-    # Moves the dataset to the data directory
-    os.makedirs(destination_path, exist_ok=True)
-    for filename in os.listdir(path):
-        src_file = os.path.join(path, filename)
-        dst_file = os.path.join(destination_path, filename)
-        if os.path.isfile(src_file):
-            os.rename(src_file, dst_file)
-            print(f"Moved {filename} to {destination_path}")
-    print(f"Creating dataframes")
+    datasets_path = download_data()
+    print("Loading datasets from: ", datasets_path)
 
     # Reddit body dataset
     df_body = pd.read_csv(
-        os.path.join(destination_path, "soc-redditHyperlinks-body.tsv"),
+        os.path.join(datasets_path, "soc-redditHyperlinks-body.tsv"),
         sep="\t",
         parse_dates=["TIMESTAMP"],
     )
 
     # Reddit title dataset
     df_title = pd.read_csv(
-        os.path.join(destination_path, "soc-redditHyperlinks-title.tsv"),
+        os.path.join(datasets_path, "soc-redditHyperlinks-title.tsv"),
         sep="\t",
         parse_dates=["TIMESTAMP"],
     )
 
     # Reddit subreddits embeddings
     df_subreddits = pd.read_csv(
-        os.path.join(destination_path, "web-redditEmbeddings-subreddits.csv"),
+        os.path.join(datasets_path, "web-redditEmbeddings-subreddits.csv"),
         names=["SUBREDDIT"] + [f"EMBEDDING_{i}" for i in range(300)],
     )
 
     # Reddit users embeddings
     df_users = pd.read_csv(
-        os.path.join(destination_path, "web-redditEmbeddings-users.csv"),
+        os.path.join(datasets_path, "web-redditEmbeddings-users.csv"),
         names=["USER"] + [f"EMBEDDING_{i}" for i in range(300)],
     )
 
@@ -53,4 +49,5 @@ def load_data():
         names=["USER"] + [f"EMBEDDING_{i}" for i in range(300)],
     )"""
 
+    print("Completed")
     return df_body, df_title, df_subreddits, df_users
